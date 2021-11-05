c9b55a18a84053778fd409fcd46eedb9b93c19ab
spuebla <pueblasalva9@gmail.com>
2021-11-04 22:27:29 -0300

    Count

---
+++ src/componentes/Count.js
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import React from 'react'
+import "../css/Container.css"
+
+const Count=({stock , initial})=> {
+  const [count, setCount] = useState(initial)
+  
+   const sumar=()=>{count < stock ? setCount(count+1) : alert ('Limite de productos')}
+   const restar=()=>{ count > initial ? setCount(count-1) : alert('Sin stock') }
+   const onAdd=()=>{
+     alert(`Agregaste ${count } productos`)
+   }
+    return (
+        <div className="count">
+          <button onClick={sumar}>+</button>
+          <label>Cantidad:{count} </label>
+          <button onClick={restar}>-</button>
+
+          <button onClick={onAdd}>Agregar</button>
+
+
+        </div>
+    )
+}
+export default Count;
--- src/componentes/ItemListContainer.js
+++ src/componentes/ItemListContainer.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import "../css/Container.css"
+import Count from './Count'
+
+
 
 export default function ItemListContainer() {
     return (
         <div ClassName="Container">
-          <p>ITEMS</p>
+          <Count initial={0} stock={5}/>
         </div>
     )
 }
